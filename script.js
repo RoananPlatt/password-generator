9275ce35cf84b46facbaf6cbe93b7d665162d075
Roanan Platt <roanan.platt@gmail.com>
2022-06-30 11:03:03 -0600

    added generatePassword function

---
+++ script.js
@@ -0,0 +1,34 @@
+// Assignment code here
+var password=document.getElementById("password");
+
+function generatePassword() {
+      var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+      var passwordLength = 12;
+      var password = "";
+  for (var i = 0; i <= passwordLength; i++) {
+    var randomNumber = Math.floor(Math.random() * chars.length);
+    password += chars.substring(randomNumber, randomNumber +1);
+    }
+          document.getElementById("password").value = password;
+}
+
+function copyPassword() {
+  var copyText = document.getElementById("password");
+  copyText.select();
+  document.execCommand("copy");  
+}
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
